b97a9ac2435786cad83b83ed82959f083081f7e2
kei-600th <kei19941005@gmail.com>
2023-07-22 16:50:59 +0900

    station06 APIを叩いて、ランダムな画像を表示しよう

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ import './App.css'
  */
 export const App = () => {
   const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/papillon/n02086910_7156.jpg")
+
+  const fetchRandomDogImage = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      setDogUrl(data.message);
+    } catch (error) {
+      console.error('Error fetching dog image:', error);
+    }
+  };
+
   return (
     <div>
       <header>
         <h1>DogsAPP</h1>
       </header>
       <img src={dogUrl} alt="Example" />
-      <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/papillon/n02086910_7609.jpg")}>犬変更</button>
+      <button onClick={fetchRandomDogImage}>犬変更</button>
       <h2>Hello, world!</h2>
     </div>
   )
